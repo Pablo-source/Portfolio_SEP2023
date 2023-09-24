10a44607a722867fd9792c7361c04672551edb93
Pablo Leon <datavizvalencia@gmail.com>
2023-09-24 21:03:56 +0100

    Add files via upload

--- ggplot2_gallery/05 Tidy up downloaded AE data.R
+++ ggplot2_gallery/05 Tidy up downloaded AE data.R
@@ -1,5 +1,12 @@
 # 05 Tidy up downloaded AE data.R
 
+# 1. Ensure Pacman is installed and loaded to proceed with the installation of required packages 
+packages <- c("pacman") 
+# Install packages not yet installed 
+installed_packages <- packages %in% rownames(installed.packages()) if (any(installed_packages == FALSE)) { install.packages(packages[!installed_packages]) } 
+# Packages loading 
+invisible(lapply(packages, library, character.only = TRUE))
+
 # 1. Load required packages 
 pacman::p_load(readxl,here,dplyr,janitor)
 
