4bf7befb298e82f3eee197abc17afe0bdf658207
Pablo Leon <datavizvalencia@gmail.com>
2023-09-24 21:39:42 +0100

    Add files via upload

--- ggplot2_gallery/13 Spaghetti plot OECD CPI 1974_2022.R
+++ ggplot2_gallery/13 Spaghetti plot OECD CPI 1974_2022.R
@@ -8,6 +8,13 @@
 
 OECD_Inflation_CPI.csv
 
+# 1. Ensure Pacman is installed and loaded to proceed with the installation of required packages 
+packages <- c("pacman") 
+# Install packages not yet installed 
+installed_packages <- packages %in% rownames(installed.packages()) if (any(installed_packages == FALSE)) { install.packages(packages[!installed_packages]) } 
+# Packages loading 
+invisible(lapply(packages, library, character.only = TRUE))
+
 # 1. Load required packages
 pacman::p_load(readxl,here,dplyr,janitor)
 
