76e26dd59f4732a3d403059c5ebe1f15db7c1ca1
Pablo Leon <datavizvalencia@gmail.com>
2023-09-25 22:51:03 +0100

    Update renv_project.R

--- Tidymodels/renv_project.R
+++ Tidymodels/renv_project.R
@@ -4,3 +4,7 @@ library(renv)
 # Record packages version usin {renv}
 # Create renv file
 renv::init()
+
+
+# To use and re-created the previous adhoc specific environmnet use this code below:
+renv:restore()
